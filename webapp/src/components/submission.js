6d436e3ef7e1393da134a5f27c674ea1ac968599
ebtesam25 <ehaque4@gmu.edu>
2022-06-02 21:45:40 -0400

    call for participation updated

--- webapp/src/components/submission.js
+++ webapp/src/components/submission.js
@@ -8,7 +8,7 @@ export default function Submission() {
       <div>
         <h1 class="text-3xl font-bold">Call for Participation</h1>
         <p class="py-6">
-The first International Workshop on Social, Cognitive, and Behaviorial Theory in Technical Research (HumanTech)  will provide an opportunity for both researchers already integrating human-centric theory into their work and researchers interested in doing so to engage with one another on the successes and opportunities across the tech spectrum. We are looking for research papers, position pieces, and experience reports. Submissions can be up to 8 pages (including references).
+The first International Workshop on Social, Cognitive, and Behaviorial Theory in Technical Research (HumanTech)  will provide an opportunity for both researchers already integrating human-centric theory into their work and researchers interested in doing so to engage with one another on the successes and opportunities across the tech spectrum. We are looking for research papers, position pieces, and experience reports. Submissions can be up to 8 pages (including references). We also welcome previously published works as non-archival submissions.
 </p>
       </div>
     </div>
